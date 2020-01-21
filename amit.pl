4c49f4c975fd56465c621e8a9dcaf507a229ca0f
bju8292 <44631080+bju8292@users.noreply.github.com>
2020-01-21 16:29:53 +0530

    Add files via upload

---
+++ amit.pl
@@ -0,0 +1,29 @@
+ #To use conditional statement in PERL
+#!/usr/bin/perl-w
+#if elsif-else
+$word = 'MINDDKL';
+if($word eq'QSTVSGE')
+{
+    print"QSTVSGE\n";
+}
+elsif($word eq'MRQQDMISHDEL')
+{
+    print"MRQQDMISHDEL\n";
+
+}
+elsif($word eq'MNIDDKL')
+{
+    print"MNIDDKL-the magic word!\n";
+}
+else
+{
+    print"Is\"$word\"a peptide?This program is not sure\n";
+}
+print"    ";
+print"\n\n";
+#exit;
+
+
+
+
+
